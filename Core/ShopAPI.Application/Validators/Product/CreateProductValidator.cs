f6103807bb2b5e47418730d7da8496f6d1a8a212
İbrahim Özdemir <ozdmirr77@gmail.com>
2024-06-09 00:02:42 +0300

    FluentValidation eklendi.

---
+++ Core/ShopAPI.Application/Validators/Product/CreateProductValidator.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation;
+using ShopAPI.Application.VMs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopAPI.Application.Validators.Product
+{
+    public class CreateProductValidator :AbstractValidator<ProductCreateVM>
+    {
+        public CreateProductValidator()
+        {
+            RuleFor(x => x.ProductName).NotEmpty().NotNull().WithMessage("Ürün adı bos olamaz").MaximumLength(300).MinimumLength(2).WithMessage("Urun adı 2 ile 300 karakter arasında olmalıdır");
+            RuleFor(x => x.ProductPrice).NotEmpty().NotNull().WithMessage("Ürün fiyatı bos olamaz").Must(x => x >= 0).WithMessage("Urun fiyatı 0'dan büyük olmalıdır");
+            RuleFor(x => x.ProductStock).NotEmpty().NotNull().WithMessage("Ürün stok bos olamaz").Must(x => x >= 0).WithMessage("Urun stok adı 0'dan büyük olmalıdır");
+        }
+
+    }
+}
--- Presentation/ShopAPI.Web/Program.cs
+++ Presentation/ShopAPI.Web/Program.cs
@@ -1,11 +1,22 @@
+using FluentValidation;
+using FluentValidation.AspNetCore;
+using ShopAPI.Application.Validators.Product;
+using ShopAPI.Infrastructure.Filters;
 using ShopAPI.Persistance;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+//builder.Services.AddControllers(opt => opt.Filters.Add<ValidationFilter>()).AddFluentValidation(opt => opt.RegisterValidatorsFromAssemblyContaining<CreateProductValidator>()).ConfigureApiBehaviorOptions(options => options.SuppressModelStateInvalidFilter = true);
+
+//builder.Services.AddFluentValidationAutoValidation().AddFluentValidationClientsideAdapters();
+//builder.Services.AddValidatorsFromAssemblyContaining<CreateProductValidator>();
+//builder.Services.AddControllers(options => options.Filters.Add<ValidationFilter>())
+//    .ConfigureApiBehaviorOptions(options => options.SuppressModelStateInvalidFilter = true);
+
+builder.Services.AddControllers(opt => opt.Filters.Add<ValidationFilter>())
+    .AddFluentValidation(fv => fv.RegisterValidatorsFromAssemblyContaining<CreateProductValidator>()).ConfigureApiBehaviorOptions(opt => opt.SuppressModelStateInvalidFilter = true);
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
